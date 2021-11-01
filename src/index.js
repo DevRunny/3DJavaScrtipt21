9e36909981f73c029d36e57b5eaba8705f01d941
Max Lebedev <dev.runny@gmail.com>
2021-11-02 00:15:35 +0300

    Завершение обязательного задания 21 урока

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
+import formsValidation from "./modules/formsValidation";
 import sliderSwiper from "./modules/sliderSwiper";
 
 timer("30 november 2021");
 menu();
 modal();
+formsValidation();
 sliderSwiper();
