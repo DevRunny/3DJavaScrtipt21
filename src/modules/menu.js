b4f9caa72bd479a7e8774a480e45767406f58739
Max Lebedev <dev.runny@gmail.com>
2021-11-04 01:45:55 +0300

    Внесение правок в условие

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,11 +1,16 @@
 const menu = () => {
   const menu = document.querySelector("menu");
+  const menuItems = menu.querySelectorAll("ul>li>a");
 
   document.addEventListener("click", (e) => {
     if (
       e.target.closest(".menu") ||
       e.target.classList.contains("close-btn") ||
-      e.target.closest("ul")
+      e.target === menuItems[0] ||
+      e.target === menuItems[1] ||
+      e.target === menuItems[2] ||
+      e.target === menuItems[3] ||
+      e.target === menuItems[4]
     ) {
       menu.classList.toggle("active-menu");
     } else if (!e.target.closest(".active-menu")) {
