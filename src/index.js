34a2f04c6141c528917ac76656762390296e3a14
Max Lebedev <dev.runny@gmail.com>
2021-11-03 22:01:53 +0300

    Реализованы табы

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
 import formsValidation from "./modules/formsValidation";
+import tabs from "./modules/tabs";
 import sliderSwiper from "./modules/sliderSwiper";
 
 timer("30 november 2021");
 menu();
 modal();
 formsValidation();
+tabs();
 sliderSwiper();
